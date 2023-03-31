f41cb243662a277761e38f0c5fca359cd0836e1f
baoloi2002 <loitan1042002@gmail.com>
2023-03-31 20:31:02 +0700

    toi da lam gi nhi

---
+++ Project_Socket/Server/Person.cs
@@ -0,0 +1,18 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Project_Socket.Server
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public PackIconKind Icon { get; set; }
+        public int Order { get; set; }
+    }
+}
+
