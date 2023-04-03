395ee172ba6a7de2748a0fb504c3b99cf0b5f399
Dong Cat <legorussiablock@gmail.com>
2023-04-03 14:29:10 +0700

    Update ClientWindow.xaml.cs

--- Project_Socket/Client/ClientWindow.xaml.cs
+++ Project_Socket/Client/ClientWindow.xaml.cs
@@ -1,26 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Text.Json;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 // TODO
 // 1. Add Game Menu: Play, Quit
 // 2. Play Window: Server IP, Port, Nickname, Connect
 // 3. If click Connect -> Game Window (which is already done)
 // 4. Win Window: Win, Quit
-
-
 
 namespace Project_Socket.Client
 {
@@ -36,8 +21,45 @@ namespace Project_Socket.Client
 
         private void Connect_Click(object sender, RoutedEventArgs e)
         {
+            //string[] userInput = txtServerAddress.Text.Split(':');
+            //string serverIP = userInput[0];
+            //int serverPort = int.Parse(userInput[1]);
+            //string nickname = txtNickname.Text;
 
+            string serverIP = "127.0.0.1";
+            int serverPort = 6294;
+            string nickname = "Bob";
+
+            // Create a TCP/IP socket by using a client object
+            Client client = new Client();
+            client.ClientConnect(serverIP, serverPort);
+
+            // Send the nickname to the server
+            //try
+            //{
+            //    // Package and send to server
+            //    Packet temp = new Packet();
+            //    //temp.AddData(nickname);
+
+            //    client.SendPacket(temp);
+            //    // Check if the nickname is valid
+            //    //if (client.ReceivePacket() == "1")
+            //    //{
+            //    //    // If the nickname is valid, go to the game page
+            //    ClientGame clientGame = new ClientGame();
+            //    this.Content = clientGame;
+            //    //}
+            //    //else
+            //    //{
+            //    //    // If the nickname is invalid, show the error message
+            //    //    MessageBox.Show("Nickname is invalid!");
+            //    //}
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show(ex.Message);
+            //}
+            
         }
     }
-
 }
